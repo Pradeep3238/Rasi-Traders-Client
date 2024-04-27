e00569af73aad09f3e22a73fd98a13973acc4b17
Pradeep3238 <d.pradeep3238@gmail.com>
2024-04-27 19:26:39 +0530

    Added cancel order functions

--- src/pages/OrdersPage.tsx
+++ src/pages/OrdersPage.tsx
@@ -1,102 +1,169 @@
 import React, { useEffect, useState } from "react";
-import {  Button, Table, Tag } from "antd";
+import { Button, Modal, Spin, Table, Tag, message } from "antd";
 import { useSelector } from "react-redux";
 
 const OrdersPage: React.FC = () => {
+  const { userData } = useSelector((state: any) => state.auth);
+  const [orderData, setOrderData] = useState();
+  const [cancelOrderId, setCancelOrderId] = useState<null | string>(null);
+  const [isCancelModalVisible, setIsCancelModalVisible] = useState(false);
+  const [isCancelling, setIsCancelling] = useState(false);
 
-  const {userData} = useSelector((state:any)=> state.auth)
-const [orderData, setOrderData] = useState();
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/api/v1/order/${userData._id}`
+        );
 
-  useEffect(()=>{
-    const fetchOrders = async()=>{
-      try{
-
-        const response =await fetch(`http://localhost:3000/api/v1/order/${userData._id}`);
-  
         if (!response.ok) {
           throw new Error("Failed to fetch orders");
         }
         const data = await response.json();
-        console.log(data)
-        
+        console.log(data);
+
         const formattedData = data.order.map((order: any) => ({
           key: order._id,
-          transactionId: order. razorpay_payment_id,
-          product: order.products.map((prod: any) => <div key={prod.product._id}>• {prod.product.name} - <span style={{fontWeight:'bold'}}>{prod.quantity}</span></div>),
+          transactionId: order.razorpay_payment_id,
+          product: order.products.map((prod: any) => (
+            <div key={prod.product._id}>
+              • {prod.product.name} -{" "}
+              <span style={{ fontWeight: "bold" }}>{prod.quantity}</span>
+            </div>
+          )),
           billAmount: order.billAmount,
           status: order.status,
-          quantity: order.totalQuantity
+          quantity: order.totalQuantity,
         }));
         setOrderData(formattedData);
-    } catch (error) {
-      console.log("Error fetching orders:", error);
+      } catch (error) {
+        console.log("Error fetching orders:", error);
+      }
+    };
+
+    fetchOrders();
+  }, [userData._id]);
+
+  console.log(orderData);
+
+  const handleCancelOrder = (transactionId: string) => {
+    setCancelOrderId(transactionId);
+    setIsCancelModalVisible(true);
+  };
+
+  const handleCancel = async () => {
+    setIsCancelling(true);
+    setIsCancelModalVisible(false);
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/v1/order/cancel/${cancelOrderId}`,
+        {
+          method: "POST",
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Failed to cancel order");
+      }
+
+      const responseData = await response.json();
+      message.success(
+        "Order cancelled successfully. You amount will be refunded in 5-7 business days"
+      );
+      const updatedOrderData = orderData.map((order: any) =>
+        order.key === cancelOrderId ? { ...order, status: "cancelled" } : order
+      );
+      setOrderData(updatedOrderData);
+      console.log("Cancellation response:", responseData);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsCancelling(false);
     }
   };
 
-  fetchOrders();
-}, [userData._id])
+  const handleCancelModalCancel = () => {
+    setIsCancelModalVisible(false);
+  };
 
-console.log(orderData)
-const columns = [
-
-  {
-    title: "Transaction Id",
-    dataIndex: "transactionId",
-    key: "transactionId",
-    render: (text: string) => <a>{text}</a>,
-  },
-  {
-    title: "Products & Quantity",
-    dataIndex: "product",
-    key: "product",
-  },
-  {
-    title: "No.Of Items",
-    dataIndex: "quantity",
-    key: "quantity",
-  },
-  {
-    title: "Bill Amount",
-    dataIndex: "billAmount",
-    key: "billAmount",
-  },
-  {
-    title: "Status",
-    key: "status",
-    dataIndex: "status",
-    render: (status: string) => {
-      let color: string;
-      switch (status) {
-        case "pending":
-          color = "red";
-          break;
-        case "processing":
-          color = "yellow";
-          break;
-        case "shipped":
-          color = "orange";
-          break;
-        case "delivered":
-          color = "green";
-          break;
-        default:
-          color = "blue"; // Default color if status is not recognized
-          break;
-      }
-      return <Tag color={color}>{status.toUpperCase()}</Tag>;
+  const columns = [
+    {
+      title: "Transaction Id",
+      dataIndex: "transactionId",
+      key: "transactionId",
+      render: (text: string) => <a>{text}</a>,
     },
-  },
-  {
-    title: "Actions",
-    dataIndex: "action",
-    key: "action",
-    render: ()=><Button type='dashed' style={{borderColor:'red'}}>Cancel Order</Button>,
-  },
-];
+    {
+      title: "Products & Quantity",
+      dataIndex: "product",
+      key: "product",
+    },
+    {
+      title: "No.Of Items",
+      dataIndex: "quantity",
+      key: "quantity",
+    },
+    {
+      title: "Bill Amount",
+      dataIndex: "billAmount",
+      key: "billAmount",
+    },
+    {
+      title: "Status",
+      key: "status",
+      dataIndex: "status",
+      render: (status: string) => {
+        let color: string;
+        switch (status) {
+          case "pending":
+            color = "grey";
+            break;
+          case "processing":
+            color = "yellow";
+            break;
+          case "shipped":
+            color = "orange";
+            break;
+          case "delivered":
+            color = "green";
+            break;
+          case "cancelled":
+            color = "red"; // Default color if status is not recognized
+            break;
+        }
+        return <Tag color={color}>{status.toUpperCase()}</Tag>;
+      },
+    },
+    {
+      title: "Actions",
+      dataIndex: "action",
+      key: "action",
+      render: (_: any, record: any) => (
+        <Button
+          type="dashed"
+          style={{ color: "red" }}
+          onClick={() => handleCancelOrder(record.transactionId)}
+          disabled={record.status ==='cancelled'}
+        >
+          Cancel Order
+        </Button>
+      ),
+    },
+  ];
 
-return (
-  <Table columns={columns} dataSource={orderData} />
-);
+  return (
+    <>
+      <Table columns={columns} dataSource={orderData} />
+      <Modal
+        title="Order cancellation"
+        visible={isCancelModalVisible}
+        onOk={handleCancel}
+        onCancel={handleCancelModalCancel}
+      >
+        <p>Are you sure you want to cancel this order?</p>
+        {isCancelling && <Spin />}
+      </Modal>
+    </>
+  );
 };
 
 export default OrdersPage;
